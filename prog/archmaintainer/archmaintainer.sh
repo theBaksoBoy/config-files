4e56dc91e0b3cb63d49b806c41cd1dd21f0dbded
baksoBoy <baksoboy@proton.me>
2026-09-25 15:00:11 +0200

    created placeholder archmaintainer script

---
+++ prog/archmaintainer/archmaintainer.sh
@@ -0,0 +1,2 @@
+echo
+echo "archmaintainer was not included in the config-files repo that you cloned, as it would risk having it very out of date in this repo. cd into this directory (~/prog/archmaintainer/), delete this file, and run git clone \"https://github.com/theBaksoBoy/archmaintainer.git\" to fix this issue, and get the most recent version of archmaintainer."
